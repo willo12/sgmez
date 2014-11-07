adca5be29ee2178b1a2ca91fed93a8c419dcb572
willo12 <wim.sijp@gmail.com>
2014-11-07 14:20:27 +1100

    Initial production ready.

--- settings.py
+++ settings.py
@@ -94,7 +94,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost','.local']
+ALLOWED_HOSTS = ['localhost','.local','willospace.pythonanywhere.com','willospace.pythonanywhere.com/sgdata']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
